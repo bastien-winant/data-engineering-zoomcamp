1708538fbfcdf5e28126a61c97a178042e8c273a
bastien-winant <armand.winant@gmail.com>
2026-01-15 22:38:59 +0100

    gcs bucket

--- 01-docker_terraform/terraform/main.tf
+++ 01-docker_terraform/terraform/main.tf
@@ -1,14 +1,29 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "7.16.0"
+      source = "hashicorp/google"
+      version = "4.72.1"
     }
   }
 }
 
 provider "google" {
-  credentials = "~/.creds/gcp/terraform-runner.json"
+  credentials = "./creds/service-account-creds.json"
   project     = "spatial-thinker-484214-n8"
-  region      = "europe-west3"
+  region      = "europe-west1"
+}
+
+resource "google_storage_bucket" "demo_bucket" {
+  name          = "terraform-demo-bucket-spatial-thinker-484214-n8"
+  location      = "europe-west1"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
