011cd61b632744c5c9321cc96bfedac420a9e7a1
bastien-winant <armand.winant@gmail.com>
2026-01-19 23:21:12 +0100

    add argument for vehicle type

--- 01-docker_terraform/homework/ingest_trips_data.py
+++ 01-docker_terraform/homework/ingest_trips_data.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 import argparse
 import duckdb
 
-def run_pipeline(user, password, host, port, db, table, year, month, chunk_size, replace=False):
-	url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet"
+def run_pipeline(user, password, host, port, db, table, vehicle_type, year, month, chunk_size, replace=False):
+	url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{vehicle_type}_tripdata_{year}-{month:02d}.parquet"
 
 	with duckdb.connect(':memory:') as con:
 		engine = create_engine(f"postgresql+psycopg://{user}:{password}@{host}:{port}/{db}")
@@ -50,6 +50,7 @@ if __name__=="__main__":
 	parser.add_argument('--pg-host', default='localhost', help='PostgreSQL host')
 	parser.add_argument('--pg-port', default='5431', help='PostgreSQL port')
 	parser.add_argument('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+	parser.add_argument('--type', default='green', help='Type of vehicle')
 	parser.add_argument('--year', default=2021, type=int, help='Year of the data')
 	parser.add_argument('--month', default=1, type=int, help='Month of the data')
 	parser.add_argument('--chunk-size', default=100000, type=int, help='Chunk size for ingestion')
@@ -63,10 +64,11 @@ if __name__=="__main__":
 	pg_host = args.pg_host
 	pg_port = args.pg_port
 	pg_db = args.pg_db
+	vehicle_type = args.type
 	year = args.year
 	month = args.month
 	chunk_size = args.chunk_size
 	target_table = args.target_table
 	replace_table = args.replace
 
-	run_pipeline(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunk_size, replace_table)
+	run_pipeline(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, vehicle_type, year, month, chunk_size, replace_table)
