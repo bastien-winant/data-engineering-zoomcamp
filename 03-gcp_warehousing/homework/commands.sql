e95b0eb76b9a036880ade5cde745cb228e4783aa
bastien-winant <armand.winant@gmail.com>
2026-02-09 18:35:20 +0100

    howework queries

--- 03-gcp_warehousing/homework/commands.sql
+++ 03-gcp_warehousing/homework/commands.sql
@@ -1,39 +1,48 @@
 -- CREATE AN EXTERNAL TABLE FROM GCP BUCKET FILES
 CREATE OR REPLACE EXTERNAL TABLE `spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata`
 OPTIONS(
-  format = 'CSV',
-  uris = ['gs://zoomcamp_spatial-thinker-484214-n8/yellow_tripdata_2019-*.csv', 'gs://zoomcamp_spatial-thinker-484214-n8/yellow_tripdata_2020-*.csv']
+  format = 'PARQUET',
+  uris = ['gs://zoomcamp_spatial-thinker-484214-n8/yellow_tripdata_2024-*.parquet']
 );
-
-SELECT * FROM spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata LIMIT 10;
 
 -- CREATE REGULAR BIGQUERY TABLE FROM EXTERNAL TABLE
 CREATE OR REPLACE TABLE spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned AS
 SELECT * FROM spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata;
 
--- CREATE PARTITIONED TABLE
+-- Question 1. Counting records
+SELECT COUNT(*) FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned;
+-- 20332093
+
+-- Question 2. Data read estimation
+SELECT COUNT (DISTINCT PULocationID) FROM spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata; -- 0 MB
+SELECT COUNT (DISTINCT PULocationID) FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned; -- 155.12 MB
+
+-- Question 3. Understanding columnar storage
+SELECT PULocationID FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned;
+SELECT PULocationID, DOLocationID FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned;
+
+-- Question 4. Counting zero fare trips
+SELECT COUNT(*) FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned WHERE fare_amount = 0;
+-- 8333
+
+-- Question 5. Partitioning and clustering
+-- Partition by tpep_dropoff_datetime and Cluster on VendorID
+
+-- Question 6. Partition benefits
 CREATE OR REPLACE TABLE spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_partitioned
-PARTITION BY DATE(tpep_pickup_datetime) AS
-SELECT * FROM spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata;
-
-
-SELECT DISTINCT(VendorID)
-FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned
-WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
-
--- CREATE PARTITIONED TABLE
-SELECT DISTINCT(VendorID)
-FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_partitioned
-WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
-
--- INSPECT PARTITIONS
-SELECT table_name, partition_id, total_rows
-FROM `zoomcamp.INFORMATION_SCHEMA.PARTITIONS`
-WHERE table_name = 'yellow_tripdata_partitioned'
-ORDER BY total_rows DESC;
-
--- CREATE CLUSTERED TABLE (ARRANGE WITHIN PARTITION)
-CREATE OR REPLACE TABLE spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_partitioned_clustered
-PARTITION BY DATE(tpep_pickup_datetime)
+PARTITION BY DATE(tpep_dropoff_datetime)
 CLUSTER BY VendorID AS
 SELECT * FROM spatial-thinker-484214-n8.zoomcamp.external_yellow_tripdata;
+
+SELECT
+	DISTINCT VendorID
+FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned
+WHERE tpep_dropoff_datetime BETWEEN '2024-03-01' AND '2024-03-15'; -- 310.24
+
+SELECT
+	DISTINCT VendorID
+FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_partitioned
+WHERE tpep_dropoff_datetime BETWEEN '2024-03-01' AND '2024-03-15'; -- 26.84
+
+-- Question 9. Understanding table scans
+SELECT count(*) FROM spatial-thinker-484214-n8.zoomcamp.yellow_tripdata_non_partitioned; -- 0
