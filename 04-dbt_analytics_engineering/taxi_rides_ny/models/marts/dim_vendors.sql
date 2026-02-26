91669f69c6659a88d2fec557b6edfc227ff5d59a
bastien-winant <armand.winant@gmail.com>
2026-02-26 23:37:10 +0100

    vendor dimension table

---
+++ 04-dbt_analytics_engineering/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -0,0 +1,10 @@
+WITH all_trips AS (
+	SELECT * FROM {{ ref("int_trips_union") }}
+),
+vendors AS (
+	SELECT
+		DISTINCT vendor_id,
+	{{ get_vendor_name('vendor_id') }} AS vendor_name
+	FROM all_trips
+)
+SELECT * FROM vendors
