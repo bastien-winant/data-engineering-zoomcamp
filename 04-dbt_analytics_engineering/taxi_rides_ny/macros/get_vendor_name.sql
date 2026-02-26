30b46d26ff010e8d1b2783f309a764c408c92fe9
bastien-winant <armand.winant@gmail.com>
2026-02-26 23:39:55 +0100

    fix get vendor name macro

--- 04-dbt_analytics_engineering/taxi_rides_ny/macros/get_vendor_name.sql
+++ 04-dbt_analytics_engineering/taxi_rides_ny/macros/get_vendor_name.sql
@@ -1,7 +1,7 @@
-{% macro get_vendor_names(vendor_id) %}
+{% macro get_vendor_name(vendor_id) %}
 CASE
 	WHEN {{ vendor_id }} = 1 THEN 'Creative Mobile Technologies, LLC'
 	WHEN {{ vendor_id }} = 2 THEN 'VeriFone Inc'
 	WHEN {{ vendor_id }} = 4 THEN 'Unknown vendor'
-END AS vendor_name
+END
 {% endmacro %}
