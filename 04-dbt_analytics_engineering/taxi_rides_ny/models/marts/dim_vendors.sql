4881a2fe1b0832468e0d51073f8b2ea2a0bbedf6
bastien-winant <armand.winant@gmail.com>
2026-03-10 23:57:50 +0100

    code cleaning and marts schema

--- 04-dbt_analytics_engineering/taxi_rides_ny/models/marts/dim_vendors.sql
+++ 04-dbt_analytics_engineering/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -4,7 +4,7 @@ WITH all_trips AS (
 vendors AS (
 	SELECT
 		DISTINCT vendor_id,
-	{{ get_vendor_name('vendor_id') }} AS vendor_name
+		{{ get_vendor_name('vendor_id') }} AS vendor_name
 	FROM all_trips
 )
 SELECT * FROM vendors
